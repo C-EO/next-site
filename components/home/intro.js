c4edea756f7abafe49a4145d99ce56c5fc93118b
Shu <ds303077135@gmail.com>
2018-09-17 11:16:41 +0800

    small tweaks

--- components/home/intro.js
+++ components/home/intro.js
@@ -72,7 +72,7 @@ export default ({}) => {
             }
             {
               <div className='campaign no-drag no-tap-highlight' style={{
-                margin: isMobile ? '2rem 0 2rem' : '1rem 0 1.6rem'
+                margin: isMobile ? '7rem 0 2rem' : '1rem 0 1.6rem'
               }}>
                 <h1 className={classNames('fw6', isMobile ? 'f2' : 'f0')}>The React Framework for</h1>
                 <h2 className={classNames(isMobile ? 'f0' : 'f-xs-0', 'campaign', 'fw7')} style={{
--- components/icons/arrow-up.js
+++ components/icons/arrow-up.js
@@ -1,8 +1,8 @@
-export default ({color}) => <svg width="12px" height="8px" viewBox="0 0 12 8" version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-    <g id="Showcase" transform="translate(-767.000000, -1776.000000)" fill={color || 'inherit'} fillRule="nonzero">
-      <g id="small-up" transform="translate(767.000000, 1776.000000)">
-        <polygon id="Shape" points="10 7.4 6 3.4 2 7.4 0.6 6 6 0.6 11.4 6"></polygon>
+export default ({color}) => <svg width="12px" height="8px" viewBox="0 0 12 8">
+  <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g transform="translate(-767.000000, -1776.000000)" fill={color || 'inherit'} fillRule="nonzero">
+      <g transform="translate(767.000000, 1776.000000)">
+        <polygon points="10 7.4 6 3.4 2 7.4 0.6 6 6 0.6 11.4 6"></polygon>
       </g>
     </g>
   </g>
