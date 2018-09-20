ea14b7db09a1bcc62cc506f3a0d09146b3cc77f1
Shu <ds303077135@gmail.com>
2018-09-20 17:59:40 +0800

    Fix sidebar height

--- components/docs/sidebar.js
+++ components/docs/sidebar.js
@@ -1,107 +1,104 @@
-import { PureComponent, Component } from 'react'
-import { Code } from './text/code'
-import _scrollIntoViewIfNeeded from 'scroll-into-view-if-needed'
-import slugify from '@sindresorhus/slugify'
+import { PureComponent, Component } from 'react';
+import { Code } from './text/code';
+import _scrollIntoViewIfNeeded from 'scroll-into-view-if-needed';
+import slugify from '@sindresorhus/slugify';
 
-import Header from '../header'
-import Container from '../container'
-import ArrowRight from '../icons/arrow-right'
+import Header from '../header';
+import Container from '../container';
+import ArrowRight from '../icons/arrow-right';
 
 const navElements = [
-  "Setup",
-  "Automatic Code Splitting",
-  "CSS",
-  "Static file serving (e.g.: images)",
-  "Populating <head>",
-  "Fetching data and component lifecycle",
-  "Routing",
-  "Prefetching Pages",
-  "Custom server and routing",
-  "Dynamic Import",
-  "Custom <App>",
-  "Custom <Document>",
-  "Custom error handling",
-  "Reusing the built-in error page",
-  "Custom configuration",
-  "Customizing webpack config",
-  "Customizing babel config",
-  "CDN support with Asset Prefix",
-  "Production deployment",
-  "Static HTML export",
-  "Usage",
-  "Limitation",
-  "Multi Zones",
-  "How to define a zone",
-  "How to merge them",
-  "Recipes",
-  "FAQ",
-  "Contributing"
-]
+  'Setup',
+  'Automatic Code Splitting',
+  'CSS',
+  'Static file serving (e.g.: images)',
+  'Populating <head>',
+  'Fetching data and component lifecycle',
+  'Routing',
+  'Prefetching Pages',
+  'Custom server and routing',
+  'Dynamic Import',
+  'Custom <App>',
+  'Custom <Document>',
+  'Custom error handling',
+  'Reusing the built-in error page',
+  'Custom configuration',
+  'Customizing webpack config',
+  'Customizing babel config',
+  'CDN support with Asset Prefix',
+  'Production deployment',
+  'Static HTML export',
+  'Usage',
+  'Limitation',
+  'Multi Zones',
+  'How to define a zone',
+  'How to merge them',
+  'Recipes',
+  'FAQ',
+  'Contributing'
+];
 
 function scrollIntoViewIfNeeded(elem) {
-  const finalElement = findClosestScrollableElement(elem)
-  return _scrollIntoViewIfNeeded(
-    elem.parentElement,
-    {
-      behavior: 'smooth',
-      scrollMode: 'if-needed',
-      block: 'center',
-      boundary: finalElement,
-    },
-  )
+  const finalElement = findClosestScrollableElement(elem);
+  return _scrollIntoViewIfNeeded(elem.parentElement, {
+    behavior: 'smooth',
+    scrollMode: 'if-needed',
+    block: 'center',
+    boundary: finalElement
+  });
 }
 
 function findClosestScrollableElement(_elem) {
-  const { parentNode } = _elem
-  if (!parentNode) return null
+  const { parentNode } = _elem;
+  if (!parentNode) return null;
 
   if (
     parentNode.scrollHeight > parentNode.clientHeight ||
     parentNode.scrollWidth > parentNode.clientWidth
   ) {
-    return parentNode
+    return parentNode;
   } else {
-    return findClosestScrollableElement(parentNode)
+    return findClosestScrollableElement(parentNode);
   }
 }
 
 export class SidebarNavItem extends Component {
   constructor() {
-    super()
+    super();
 
-    this.activeNavItem = null
+    this.activeNavItem = null;
   }
 
   componentDidMount() {
-    this.scrollIntoViewIfNeeded()
+    this.scrollIntoViewIfNeeded();
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return this.props.isActive !== nextProps.isActive
+    return this.props.isActive !== nextProps.isActive;
   }
 
   componentDidUpdate() {
-    this.scrollIntoViewIfNeeded()
+    this.scrollIntoViewIfNeeded();
   }
 
   scrollIntoViewIfNeeded() {
     if (this.props.isActive) {
       if (this.activeNavItem.scrollIntoViewIfNeeded) {
-        this.activeNavItem.scrollIntoViewIfNeeded()
+        this.activeNavItem.scrollIntoViewIfNeeded();
       } else {
-        scrollIntoViewIfNeeded(this.activeNavItem)
+        scrollIntoViewIfNeeded(this.activeNavItem);
       }
     }
   }
 
   render() {
-    const {item, updateSelected, isActive, isMobile} = this.props
+    const { item, updateSelected, isActive, isMobile } = this.props;
 
     return (
       <li>
-        <a 
-          href={`#${slugify(item)}`} 
-          onClick={updateSelected} 
+        <a
+          href={`#${slugify(item)}`}
+          onClick={updateSelected}
           className={`${isActive ? 'active' : ''} f5`}
           ref={ref => (this.activeNavItem = ref)}
         >
@@ -118,7 +115,7 @@ export class SidebarNavItem extends Component {
             display: inline-block;
             color: inherit;
             padding: 6px 3px;
-            ${isMobile ? 'width: 100%;' : ''}
+            ${isMobile ? 'width: 100%;' : ''};
           }
           a:hover {
             color: gray;
@@ -129,136 +126,159 @@ export class SidebarNavItem extends Component {
           }
         `}</style>
       </li>
-    )
+    );
   }
 }
 
 export default class Sidebar extends PureComponent {
   state = {
     dropdown: false
-  }
-  updateSelected = (hash) => {
-    this.props.updateSelected(hash)
-    this.setState({ dropdown: false })
-  }
+  };
+  updateSelected = hash => {
+    this.props.updateSelected(hash);
+    this.setState({ dropdown: false });
+  };
   toggleDropdown = () => {
-    this.setState({ dropdown: !this.state.dropdown })
-  }
+    this.setState({ dropdown: !this.state.dropdown });
+  };
   render() {
-    const { isMobile } = this.props
-    const { dropdown } = this.state
+    const { isMobile } = this.props;
+    const { dropdown } = this.state;
 
     if (isMobile) {
-      const currentItem = navElements.filter(item => this.props.currentSelection === `#${slugify(item)}`)[0]
-      return <>
-        <div className='negative-spacer'>
-          <Header height={48} zIndex={999} offset={64 + 32} distance={1} defaultActive shadow>
-            <div className="docs-select f5 fw6" onClick={this.toggleDropdown}>
-              <Container>
-              <span style={{ 
-                verticalAlign: 'middle',
-                marginRight: '0.2rem',
-                display: 'inline-block',
-                lineHeight: '1rem'
-              }}><ArrowRight/></span>{currentItem}
-              </Container>
-            </div>
-            <div className={`documentation__sidebar docs-dropdown ${dropdown ? '' : ' docs-closed'}`}>
-              <Container>
-                <nav>
-                  <span className="documentation__sidebar-heading f6 fw6">Getting Started</span>
-                  <ul>
-                    {
-                      navElements.map((item, i) => (
-                        <SidebarNavItem 
-                          key={i} 
-                          item={item} 
-                          updateSelected={() => 
+      const currentItem = navElements.filter(
+        item => this.props.currentSelection === `#${slugify(item)}`
+      )[0];
+      return (
+        <>
+          <div className="negative-spacer">
+            <Header
+              height={48}
+              zIndex={999}
+              offset={64 + 32}
+              distance={1}
+              defaultActive
+              shadow
+            >
+              <div className="docs-select f5 fw6" onClick={this.toggleDropdown}>
+                <Container>
+                  <span
+                    style={{
+                      verticalAlign: 'middle',
+                      marginRight: '0.2rem',
+                      display: 'inline-block',
+                      lineHeight: '1rem'
+                    }}
+                  >
+                    <ArrowRight />
+                  </span>
+                  {currentItem}
+                </Container>
+              </div>
+              <div
+                className={`documentation__sidebar docs-dropdown ${
+                  dropdown ? '' : ' docs-closed'
+                }`}
+              >
+                <Container>
+                  <nav>
+                    <span className="documentation__sidebar-heading f6 fw6">
+                      Getting Started
+                    </span>
+                    <ul>
+                      {navElements.map((item, i) => (
+                        <SidebarNavItem
+                          key={i}
+                          item={item}
+                          updateSelected={() =>
                             this.updateSelected(`#${slugify(item)}`)
-                          } 
-                          isActive={this.props.currentSelection === `#${slugify(item)}`} 
+                          }
+                          isActive={
+                            this.props.currentSelection === `#${slugify(item)}`
+                          }
                           isMobile={true}
                         />
-                      ))
-                    }
-                  </ul>
-                </nav>
-              </Container>
-            </div>
-          </Header>
-          <style jsx>{`
-            .docs-select {
-              height: 3rem;
-              width: 100%;
-              border-top: 1px solid #f5f5f5;
-              line-height: 3rem;
-              text-align: left;
-              cursor: pointer;
-            }
-            .docs-select img {
-              vertical-align: middle;
-              margin-top: -2px;
-            }
-            .docs-dropdown {
-              position: absolute;
-              left: 0;
-              right: 0;
-              top: 100%;
-              bottom: -50vh;
-              background: white;
-              box-shadow: 0 10px 20px rgba(0, 0, 0, .1);
-              transition: bottom .5s ease;
-              overflow-y: auto;
-              -webkit-overflow-scrolling: touch;
-            }
-            .docs-dropdown.docs-closed {
-              bottom: 100%;
-            }
-            .documentation__sidebar nav {
-              padding-left: 24px;
-            }
-            .documentation__sidebar nav ul {
-              margin: 0;
-              padding: 0;
-            }
-            .documentation__sidebar-heading {
-              display: inline-block;
-              margin-top: 1rem;
-              margin-bottom: 12px;
-              margin-left: 3px;
-              color: #999999;
-              text-transform: uppercase;
-            }
-            .negative-spacer {
-              margin: 0 -1rem;
+                      ))}
+                    </ul>
+                  </nav>
+                </Container>
+              </div>
+            </Header>
+            <style jsx>{`
+              .docs-select {
+                height: 3rem;
+                width: 100%;
+                border-top: 1px solid #f5f5f5;
+                line-height: 3rem;
+                text-align: left;
+                cursor: pointer;
+              }
+              .docs-select img {
+                vertical-align: middle;
+                margin-top: -2px;
+              }
+              .docs-dropdown {
+                position: absolute;
+                left: 0;
+                right: 0;
+                top: 100%;
+                bottom: -50vh;
+                background: white;
+                box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+                transition: bottom 0.5s ease;
+                overflow-y: auto;
+                -webkit-overflow-scrolling: touch;
+              }
+              .docs-dropdown.docs-closed {
+                bottom: 100%;
+              }
+              .documentation__sidebar nav {
+                padding-left: 24px;
+              }
+              .documentation__sidebar nav ul {
+                margin: 0;
+                padding: 0;
+              }
+              .documentation__sidebar-heading {
+                display: inline-block;
+                margin-top: 1rem;
+                margin-bottom: 12px;
+                margin-left: 3px;
+                color: #999999;
+                text-transform: uppercase;
+              }
+              .negative-spacer {
+                margin: 0 -1rem;
+              }
+            `}</style>
+          </div>
+          <style jsx global>{`
+            :global(.target.docs-anchor-target) {
+              margin-top: -208px;
+              padding-top: 208px;
             }
           `}</style>
-        </div>
-        <style jsx global>{`
-          :global(.target.docs-anchor-target) {
-            margin-top: -208px;
-            padding-top: 208px;
-          }
-        `}</style>
-      </>
+        </>
+      );
     }
 
     return (
       <div className="documentation__sidebar">
         <nav>
-          <span className="documentation__sidebar-heading">Getting Started</span>
+          <span className="documentation__sidebar-heading">
+            Getting Started
+          </span>
           <ul>
-            {
-              navElements.map((item, i) => (
-                <SidebarNavItem 
-                  key={i} 
-                  item={item} 
-                  updateSelected={() => 
-                    this.props.updateSelected(`#${slugify(item)}`)
-                  } 
-                  isActive={this.props.currentSelection === `#${slugify(item)}`} />
-              ))
-            }
+            {navElements.map((item, i) => (
+              <SidebarNavItem
+                key={i}
+                item={item}
+                updateSelected={() =>
+                  this.props.updateSelected(`#${slugify(item)}`)
+                }
+                isActive={this.props.currentSelection === `#${slugify(item)}`}
+              />
+            ))}
           </ul>
         </nav>
 
@@ -278,7 +298,7 @@ export default class Sidebar extends PureComponent {
             flex-direction: column;
             width: 18rem;
             padding: 2rem 1rem 0 0;
-            height: calc(100vh - ${64 + 32}px);
+            height: calc(100vh - 64px);
           }
 
           .documentation__sidebar nav ul {
@@ -291,7 +311,7 @@ export default class Sidebar extends PureComponent {
             text-transform: uppercase;
             margin-bottom: 12px;
           }
-          
+
           // CSS only media query for mobile + SSR
           @media screen and (max-width: 640px) {
             .documentation__sidebar nav {
@@ -301,7 +321,7 @@ export default class Sidebar extends PureComponent {
             }
           }
         `}</style>
-    </div>
-    )
+      </div>
+    );
   }
 }
