827940275a522d50eb289a2f9756ad91c65a6a39
Shu <ds303077135@gmail.com>
2018-09-21 19:16:47 +0800

    Improve style

--- components/blog/with-post.js
+++ components/blog/with-post.js
@@ -30,6 +30,7 @@ const Author = meta => (
         display: inline-flex;
         align-items: center;
         padding: 0 1rem;
+        margin-bottom: 0.5rem;
         white-space: nowrap;
       }
       img {
@@ -101,10 +102,11 @@ export default meta => ({ children }) => {
           // CSS only media query for mobile
           @media screen and (max-width: 640px) {
             .authors {
-              text-align: left;
+              display: flex;
+              flex-direction: column;
+              align-items: center;
               margin-left: -1rem;
               margin-right: -1rem;
-              font-size: 14px;
             }
           }
         `}</style>
